b1e0a53d8de07542bd0051d2471ebced40440f09
Ankur Atri <ankuratri.in@gmail.com>
2023-03-26 21:42:50 +0530

    bootstrap file added

--- mfe-react-vite/container/src/main.jsx
+++ mfe-react-vite/container/src/main.jsx
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("_containerApp_root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import("./bootstrap");
